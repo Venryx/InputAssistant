41e8893a9df75e06ababbd284bff99adac78babe
Venryx <venryx@gmail.com>
2017-12-28 04:20:10 -0800

    * Next breakthrough! Finally managed to get a code block that, under certain circumstances (dragging app in window mode over click zone?), does clicking each second on its own!

--- Test1.py
+++ Test1.py
@@ -1,5 +1,6 @@
 import time
 import sys
+from random import *
 
 import uinput
 
@@ -11,28 +12,38 @@ def main():
     uinput.BTN_RIGHT,
     '''
     events = (
-        uinput.ABS_MT_TRACKING_ID,
-        uinput.ABS_MT_POSITION_X,
-        uinput.ABS_MT_POSITION_Y,
+        uinput.ABS_MT_TRACKING_ID + (0, 65535, 0, 0),
+        #(0x03, 0x39, 0, 65535, 0, 0),
+        uinput.ABS_MT_POSITION_X + (0, 1920, 0, 0),
+        #(0x03, 0x35, 0, 1920, 0, 0),
+        uinput.ABS_MT_POSITION_Y + (0, 1080, 0, 0),
+        #(0x03, 0x36, 0, 1080, 0, 0),
         uinput.ABS_MT_PRESSURE,
-        uinput.ABS_MT_TOUCH_MAJOR,
+        uinput.ABS_MT_TOUCH_MAJOR + (0, 15, 0, 0),
+        #(0x03, 0x30, 0, 15, 0, 0),
         uinput.BTN_TOUCH,
         uinput.ABS_X,
         uinput.ABS_Y,
         uinput.ABS_PRESSURE,
-        uinput.ABS_MT_PRESSURE,
+        uinput.ABS_MT_PRESSURE + (0, 255, 0, 0),
+        #(0x03, 0x3a, 0, 255, 0, 0),
+        uinput.ABS_MT_SLOT + (0, 9, 0, 0),
+        #(0x03, 0x2f, 0, 9, 0, 0),
 
-        #uinput.ABS_MT_SLOT,
+        #(0x0, 0),
+        #(0x0, 2),
+
         #uinput.SYN_REPORT,
-        #uinput.BTN_LEFT,  
+        uinput.BTN_LEFT,  
     )
 
     x = int(sys.argv[1])
     y = int(sys.argv[2])
     print "X:" + str(x) + "; Y:" + str(y)
 
-    with uinput.Device(events) as device:
+    with uinput.Device(events, "FakeTouchScreen", 0x06) as device:
         print "Device: " + str(device)
+        global_tracking_id = 1
         for i in range(20 * 1000):
             # syn=False to emit an "atomic" (5, 5) event.
             '''device.emit(uinput.REL_X, 5, syn=False)
@@ -92,54 +103,68 @@ def main():
             device.emit(uinput.ABS_Y, y, syn=False)
             device.emit(uinput.ABS_PRESSURE, 71, syn=False)
             device.syn()'''
+            
 
-            device.emit(uinput.ABS_MT_TRACKING_ID, 112, syn=False)
+            '''device.emit(uinput.ABS_MT_SLOT, 0, syn=False)
+            device.emit(uinput.ABS_MT_TRACKING_ID, 0, syn=False)
+            device.emit(uinput.BTN_TOUCH, 1, syn=False)
             device.emit(uinput.ABS_MT_POSITION_X, x, syn=False)
             device.emit(uinput.ABS_MT_POSITION_Y, y, syn=False)
-            device.emit(uinput.ABS_MT_PRESSURE, 87, syn=False)
-            device.emit(uinput.ABS_MT_TOUCH_MAJOR, 31, syn=False)
-            #device.emit_click(uinput.BTN_TOUCH, syn=False)
-            device.emit(uinput.BTN_TOUCH, 1, syn=False)
-            device.emit(uinput.ABS_X, x, syn=False)
-            device.emit(uinput.ABS_Y, y, syn=False)
-            device.emit(uinput.ABS_PRESSURE, 87, syn=False)
-            device.syn()
-            time.sleep(.926169 - .926149)
-            device.emit(uinput.ABS_MT_PRESSURE, 89, syn=False)
-            device.emit(uinput.ABS_PRESSURE, 89, syn=False)
-            device.syn()
-            time.sleep(.930645 - .926169)
-            device.emit(uinput.ABS_MT_PRESSURE, 91, syn=False)
-            device.emit(uinput.ABS_PRESSURE, 91, syn=False)
-            device.syn()
-            time.sleep(.937608 - .930645)
-            device.emit(uinput.ABS_MT_PRESSURE, 93, syn=False)
-            device.emit(uinput.ABS_PRESSURE, 93, syn=False)
-            device.syn()
-            time.sleep(.944590 - .937608)
-            device.emit(uinput.ABS_MT_PRESSURE, 94, syn=False)
-            device.emit(uinput.ABS_PRESSURE, 94, syn=False)
-            device.syn()
-            time.sleep(.951592 - .944590)
-            device.emit(uinput.ABS_MT_PRESSURE, 93, syn=False)
-            device.emit(uinput.ABS_PRESSURE, 93, syn=False)
-            device.syn()
-            time.sleep(.958621 - .951592)
-            device.emit(uinput.ABS_MT_PRESSURE, 88, syn=False)
-            device.emit(uinput.ABS_MT_TOUCH_MAJOR, 30, syn=False)
-            device.emit(uinput.ABS_PRESSURE, 88, syn=False)
-            device.syn()
-            time.sleep(.971123 - .958621)
-            device.emit(uinput.ABS_MT_TRACKING_ID, -1, syn=False)
             device.emit(uinput.BTN_TOUCH, 0, syn=False)
-            device.emit(uinput.ABS_PRESSURE, 0, syn=False)
             device.syn()
+            
+            device.emit(uinput.ABS_MT_TRACKING_ID, -1, syn=False)
+            device.syn()
+            
+            device.emit_click(uinput.BTN_LEFT, syn=False)
+            device.syn()'''
+            
+            device.emit(uinput.ABS_MT_TRACKING_ID, global_tracking_id, syn=False);
+            global_tracking_id += 1;
+            device.emit(uinput.ABS_MT_POSITION_X, x, syn=False);
+            device.emit(uinput.ABS_MT_POSITION_Y, y, syn=False);
+            device.emit(uinput.ABS_MT_PRESSURE, 127, syn=False);
+            device.emit(uinput.ABS_MT_TOUCH_MAJOR, 127, syn=False);
+            device.emit(uinput.ABS_MT_WIDTH_MAJOR, 4, syn=False);
+            #device.emit(uinput.SYN_MT_REPORT, 0, syn=False);
+            device.emit((0x0, 2), 0, syn=False);
+            device.emit(uinput.BTN_TOUCH, 1, syn=False);
+            #device.emit(uinput.SYN_REPORT, 0, syn=False);
+            device.emit(uinput.ABS_X, x, syn=False);
+            device.emit(uinput.ABS_Y, y, syn=False);
+            device.emit((0x0, 0), 0, syn=False);
+            #device.syn()
+            
+            device.emit(uinput.ABS_MT_SLOT, 0, syn=False)
+            device.emit(uinput.ABS_MT_TRACKING_ID, 0, syn=False)
+            device.emit(uinput.BTN_TOUCH, 1, syn=False)
+            device.emit(uinput.ABS_MT_POSITION_X, x, syn=False)
+            device.emit(uinput.ABS_MT_POSITION_Y, y, syn=False)
+            device.emit(uinput.BTN_TOUCH, 0, syn=False)
+            device.syn()
+            device.emit(uinput.ABS_MT_TRACKING_ID, -1, syn=False)
+            device.syn()
+            
+            device.emit(uinput.ABS_MT_SLOT, 1, syn=False)
+            device.emit(uinput.ABS_MT_TRACKING_ID, 0, syn=False)
+            device.emit(uinput.BTN_TOUCH, 1, syn=False)
+            device.emit(uinput.ABS_MT_POSITION_X, x, syn=False)
+            device.emit(uinput.ABS_MT_POSITION_Y, y, syn=False)
+            device.emit(uinput.BTN_TOUCH, 0, syn=False)
+            device.syn()
+            device.emit(uinput.ABS_MT_TRACKING_ID, -1, syn=False)
+            device.syn()
+            
+            device.emit(uinput.ABS_MT_TRACKING_ID, -1, syn=False);
+            device.emit((0x0, 2), 0, syn=False);
+            device.emit(uinput.BTN_TOUCH, 0, syn=False);
+            device.emit((0x0, 0), 0, syn=False);
 
 
             # Just for demonstration purposes: shows the motion. In real
             # application, this is of course unnecessary.
-           # time.sleep(0.01)
-            time.sleep(.005)
+            #time.sleep(0.01)
+            time.sleep(1)
             #time.sleep(0.2)
 
             #time.sleep(1)
